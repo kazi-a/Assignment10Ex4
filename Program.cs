c3e27855bd11ee490d28663e7adf6dfe1be32d06
kazia <kazia@MK>
2024-04-14 20:40:22 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Assignment10Ex4
+{
+    internal class Alarm { }
+    internal class Teeth { }
+    internal class Shower { }
+    internal class Dress { }
+    internal class Drive { }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                TurnOffAlarm();
+                Console.WriteLine("Out of bed and ready to go");
+
+                BrushTeeth();
+                Console.WriteLine("Teeth are clean");
+
+                Shower shower = await TakeAShowerAsync();
+                Console.WriteLine("Showering is done");
+
+                GetDressed();
+                Console.WriteLine("Dressed and ready to go");
+
+                Drive drive = await DriveToWorkAsync(10);
+                Console.WriteLine("Walking into work.");
+
+                Console.WriteLine("Ready to begin");
+                sw.Stop();
+                Console.WriteLine("The program took " + sw.ElapsedMilliseconds + " milliseconds to run");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"An InvalidOperationException has occurred: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An Exception has occurred: {ex.Message}");
+            }
+        }
+
+        static void TurnOffAlarm()
+        {
+            Console.WriteLine("Turned off alarm");
+        }
+
+        private static void BrushTeeth() => Console.WriteLine("Putting paste on the brush... Brushing");
+
+        private static async Task<Shower> TakeAShowerAsync()
+        {
+            Console.WriteLine("Warming the water...");
+            await Task.Delay(1000);
+            Console.WriteLine($"Shampoo and Condition the hair");
+            Console.WriteLine("Rinse off");
+            await Task.Delay(2000);
+            Console.WriteLine("Dry off");
+            // Simulate error
+            throw new InvalidOperationException("Error occurred while taking a shower.");
+            return new Shower();
+        }
+
+        private static void GetDressed() => Console.WriteLine("Together and ready to go");
+
+        private static async Task<Drive> DriveToWorkAsync(int time)
+        {
+            Console.WriteLine("Starting the car...");
+            for (int i = 0; i < time; i++)
+            {
+                Console.WriteLine($"Driving...");
+            }
+            await Task.Delay(1000);
+            Console.WriteLine("Arrived at work");
+            return new Drive();
+        }
+    }
+}
